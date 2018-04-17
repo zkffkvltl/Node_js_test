4526e5661ed60b2dd80572d9c797002b6bb7dd66
zkffkvltl <zkffkvltl@gmail.com>
2018-04-17 10:44:07 +0900

    second commit

---
+++ circle.js
@@ -0,0 +1,8 @@
+const PI=Math.PI;
+exports.area=function(r){
+    return PI*r*r;
+}
+exports.circum=function(r){
+    return 2*PI*r;
+}
+exports.pi=PI;
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log("Hello, world");
 console.log('Node.js version: '+process.version);
 console.log(process.memoryUsage());
+console.log(process.env);
+console.log(module);
+
+const circle=require('./circle'); //circle(.js)
+console.log('Area of a circle = '+circle.area(3));
+console.log('Circumference of a circle = '+circle.circum(3));
+console.log('PI= '+circle.pi);
