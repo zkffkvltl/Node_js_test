6b38666b9a9784af17ab7e4c10bcd9672e6096fc
zkffkvltl <zkffkvltl@gmail.com>
2018-04-17 11:22:02 +0900

    second commit

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ console.log('Circumference of a circle = '+circle.circum(3));
 console.log('PI= '+circle.pi);
 
 const square=require('./square');
-let mySquare=square(4);
+let mySquare=square(5);
+mySquare=square(10);
 console.log('Area of a square = '+mySquare.area());
+console.log('Circumference of a squaer = '+mySquare.circum());
--- square.js
+++ square.js
@@ -1,7 +1,12 @@
 module.exports=function(width){
     return {
+        width: width,
         area: function(){
-            return width*width;
+            return this.width*this.width;
+        },
+        circum: function(){
+            return 4*this.width;
         }
+
     };
 }
