43a5779c0f1bb65c5f16d75558f333725b74b063
zkffkvltl <zkffkvltl@gmail.com>
2018-04-17 10:57:31 +0900

    second commit

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const circle=require('./circle'); //circle(.js)
 console.log('Area of a circle = '+circle.area(3));
 console.log('Circumference of a circle = '+circle.circum(3));
 console.log('PI= '+circle.pi);
+
+const square=require('./square');
+let mySquare=square(4);
+console.log('Area of a square = '+mySquare.area());
