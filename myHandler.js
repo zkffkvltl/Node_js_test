613a1f60f42d2790ea21161abc131d8b50e9ec78
zkffkvltl <zkffkvltl@gmail.com>
2018-05-01 11:15:51 +0900

    Node_js_test

--- myHandler.js
+++ myHandler.js
@@ -1,8 +1,13 @@
-function start(){
-    return 'hello, start';
+function start(res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('hello, start');
+    res.end();
 }
-function hello(){
-    return 'hello, Hello';
+
+function hello(res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write('Hello');
+    res.end();
 }
 exports.start = start;
 exports.hello = hello;
--- myRouter.js
+++ myRouter.js
@@ -1,8 +1,12 @@
-function route(pathname){
+function route(pathname, handle, res){
     console.log('Routing for '+pathname);
     if(typeof handle[pathname]==='function')
-        return handle[pathname]();
-    else return '404 not found';
+        return handle[pathname](res);
+    else {
+        res.writeHead(404, {'Content-Type':'text/html'});
+    res.write('404 not found');
+    res.end();
+    }
 }
 
 exports.route = route;
